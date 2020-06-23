3ade656408344386f9ec92df23e6ec6150fa321e
Will <william.ducta@techcellar.com>
2020-06-23 18:15:17 +0800

    First commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const exphbs = require("express-handlebars");
+const connectDb = require("./config/db");
+
+// Load Env files
+dotenv.config({ path: "./config/config.env" });
+connectDb();
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// Request Loggings
+if (process.env === "development") {
+  app.use(morgan("dev"));
+}
+
+// Express Handlebars
+app.engine(".hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", ".hbs");
+
+// Routes
+app.use("/", require("./routes/index"));
+
+app.listen(port, () => {
+  console.log(`Server is up. Connected in port ${port}`);
+});
---
+++ routes/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+
+// @desc Login / Landing page
+// @route /
+router.get("/", (req, res) => {
+  res.render("login");
+});
+
+// @desc Dashboard
+// @route /dashboard
+router.get("/dashboard", (req, res) => {
+  res.render("dashboard");
+});
+
+module.exports = router;

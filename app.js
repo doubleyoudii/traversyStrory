a87b63c6b42a7f6f797a49c7af2a241322c0ee68
Will <william.ducta@techcellar.com>
2020-06-24 18:10:10 +0800

    passport and session init

--- app.js
+++ app.js
@@ -3,10 +3,17 @@ const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const exphbs = require("express-handlebars");
+const passport = require("passport");
+const session = require("express-session");
 const connectDb = require("./config/db");
 
 // Load Env files
 dotenv.config({ path: "./config/config.env" });
+
+// Passport Config
+require("./config/passport")(passport);
+
+// Database Connection
 connectDb();
 
 const port = process.env.PORT || 5000;
@@ -21,6 +28,19 @@ if (process.env.NODE_ENV === "development") {
 // Express Handlebars
 app.engine(".hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", ".hbs");
+
+// Sessions from express
+app.use(
+  session({
+    secret: "StoryBook",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Static Files
 app.use(express.static(path.join(__dirname, "public")));

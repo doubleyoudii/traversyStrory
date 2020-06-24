3c20fe18851d7078295593a412e4a55bc0201038
Will <william.ducta@techcellar.com>
2020-06-24 18:40:15 +0800

    adduser modle

---
+++ models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const UserSchema = mongoose.Schema({
+  goodleId: {
+    type: String,
+    required: true,
+  },
+  displayName: {
+    type: String,
+    required: true,
+  },
+  firstname: {
+    type: String,
+    required: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("User", UserSchema);

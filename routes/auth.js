b1773a072b6f09230e4ae76b961c9a2d4b52e5f4
Will <william.ducta@techcellar.com>
2020-06-26 11:53:22 +0800

    add auth.js

---
+++ routes/auth.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+const passport = require("passport");
+
+// desc Google-Login
+//route GET /auth/google
+router.get(
+  "/google",
+  passport.authenticate("google", { scope: ["profile", "openid"] })
+);
+
+// desc Google auth call back
+// route GET /auth/google/callbacck
+router.get(
+  "/google/callback",
+  passport.authenticate("google", { failureRedirect: "/" }),
+  (req, res) => {
+    res.redirect("/dashboard");
+  }
+);
+
+module.exports = router;

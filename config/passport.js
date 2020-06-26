ff1cc8d4530363051e0da666fd8b4402168839f0
Will <william.ducta@techcellar.com>
2020-06-26 14:34:18 +0800

    change key to callbackURL

--- config/passport.js
+++ config/passport.js
@@ -8,7 +8,7 @@ module.exports = function (passport) {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callback: "/auth/google/callback",
+        callbackURL: "/auth/google/callback",
       },
       async (accessToken, refreshToken, profile, done) => {
         console.log(profile);

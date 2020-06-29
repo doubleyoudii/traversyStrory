788d9656bbffa710ae530338bebf00a492ec05bf
Will <william.ducta@techcellar.com>
2020-06-29 11:41:44 +0800

    Create auth middleware

---
+++ middleware/auth.js
@@ -0,0 +1,19 @@
+module.exports = {
+  ensureAuth : function(req, res, next)  {
+    // if the user is not loged in and try to access other paged
+    if (req.isAuthenticated()) {
+      return next();
+    } else {
+      res.redirect("/"); // Login
+    }
+  },
+  ensureGuest = function(req, res, next) {
+    // if the user is alredy Loged in and try to go to "login Page", he will redirected to "dashboard"
+
+    if (req.isAuthenticated()){
+      res.redirect('/dashboard')
+    } else {
+      return next;
+    }
+  }
+}

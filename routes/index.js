dcb9e329125824d012c69ad689fbafc9faa6e9d7
Will <william.ducta@techcellar.com>
2020-07-01 16:20:43 +0800

    modify table

--- routes/index.js
+++ routes/index.js
@@ -22,8 +22,8 @@ router.get("/dashboard", ensureAuth, async (req, res) => {
       stories,
     });
   } catch (err) {
-    console.error(err);
     res.render("error/500");
+    console.error(err);
   }
 });
 
